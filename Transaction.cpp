6ee1608b4436535c2c3da6c85c65b528ee5bc625
Emre Ayözcan <emreyozcan16@gmail.com>
2026-06-13 16:10:40 +0300

    feat: İşlemler için sınıf ve metotlar oluşturuldu.

--- Transaction.cpp
+++ Transaction.cpp
@@ -1,1 +1,40 @@
 #include "Transaction.h"
+#include <iostream>
+
+//Yapıcı metot için default atamalar yapıldı
+Transaction::Transaction()
+{
+    tutar=0;
+    aciklama="";
+    tarih="";
+    kategori="";
+    gelirMi=true;
+}
+
+//Yapıcı metot parameterleri şekilde çağırılırsa atamaların yapılması için
+Transaction::Transaction(double tutar, std::string aciklama, std::string tarih, std::string kategori, bool gelirMi)
+{
+    this->tutar=tutar;
+    this->aciklama=aciklama;
+    this->tarih=tarih;
+    this->kategori=kategori;
+    this->gelirMi=gelirMi;
+}
+
+//Private kısmındaki değişkenleri sadece okuyabilmek içinb gerekli metotlar
+double Transaction::getTutar(){return tutar;}
+std::string Transaction::getAciklama(){return aciklama;}
+std::string Transaction::getTarih(){return tarih;}
+std::string Transaction::getKategori(){return kategori;}
+bool Transaction::getGelirMi(){return gelirMi;}
+
+
+void Transaction::bilgiyazdir()
+{
+    //Bilgilerin hepsinni bir arada düzgün şekilde görebilmek için
+    //Son değişkende Ternary operaötürü kullandım (kosul) ? "Doğruysa Çıkacak Sonuç" : "Yanlışsa Çıkacak Sonuç"
+    //Beklenen örenk çıktıo formatı: [13.06.2026] Market - Gıda | Tutar: 250 TL | Tip: Gider
+    std::cout<<"["<<getTarih()<<"]"<<" "<<getKategori()<<"|"<<" "<<"Tutar:"<<" "<< getTutar()<<" TL"<<" | "<<"Tip: "<<(getGelirMi() ? "Gelir" : "Gider")<<std::endl;
+}
+
+
--- Transaction.h
+++ Transaction.h
@@ -1,4 +1,34 @@
 #ifndef FINANS_ASISTANI_TRANSACTION_H
 #define FINANS_ASISTANI_TRANSACTION_H
+#include <string>
+
+//İşlmeler adında ana bir sınıf oluşturuyoruz kullanıcının işlemle değerlerini değişltirmek için bu sınıf kullanılcak
+class Transaction
+{
+    //işlem esanında kullanılcak ve önemli bilgileri private yapıyoruz ki dışardan erişim olmasın. güvenlik için
+    private:
+        double tutar;
+        std::string aciklama,tarih, kategori;
+        bool gelirMi;
+
+
+    public:
+    //Constructor metot yani yapıcı metot bunu kullanmamızın sebebi bu class oluşturulduğu zaman otomatik şekilde yerlerine deafult değer atmaktır
+    Transaction();
+    //İşlemleri yönetecek ana metot. yapıcı metot ile işlemleri yönetecek metotların adı aynı olsada derleyici hangiğsini kullancağını parametrlerden kendi anlar
+    Transaction(double tutar, std::string aciklama,std::string tarih,std::string kategori,bool gelirMi);
+
+    //Program akışında herhangi bir yerde bu değerleri okumamız gerektiği zaman sadece okumamızı sağlayacak metotlar lazımdır.
+    double getTutar();
+    std::string getAciklama();
+    std::string getTarih();
+    std::string getKategori();
+    bool getGelirMi();
+
+    //Genel işlem bilgisini yazdırmak için metot
+    void bilgiyazdir();
+
+};
+
 
 #endif //FINANS_ASISTANI_TRANSACTION_H
