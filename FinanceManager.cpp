8504dfa8548beac04ee3fe716ec80ac20a63f62c
Emre Ayözcan <emreyozcan16@gmail.com>
2026-06-13 17:17:03 +0300

    feat: FinanceManager sınıfı ve bakiye yönetimi mimarisi tamamlandı

--- FinanceManager.cpp
+++ FinanceManager.cpp
@@ -1,5 +1,38 @@
-//
-// Created by Emre on 13.06.2026.
-//
-
 #include "FinanceManager.h"
+#include "Transaction.h"
+
+//Yapıcı metot tanımı.
+//Oluşturlan anabkiye değişkenine listede yer açılıken kendi tipine göre default değert verdik
+FinanceManager::FinanceManager()
+{
+    anaBakiye = 0;
+}
+
+//işlemleri verileri tutucak listenin içine verileri eklemek için
+void FinanceManager::islemEkle(Transaction yeniIslem)
+{
+    //Listenin sonuna ekleme yapıyor
+    islemListesi.push_back(yeniIslem);
+
+    //Ekleme yaparken tutara ve gelir mi gider mi olduığuna zaten eklenmiş olan anabakiyede değişiklik yapıyor
+    if (yeniIslem.getGelirMi()==true)
+    {
+        this->anaBakiye=this->anaBakiye+yeniIslem.getTutar();
+    }
+    else
+    {
+        this->anaBakiye=this->anaBakiye-yeniIslem.getTutar();
+    }
+}
+
+void FinanceManager::gecmisiListele()
+{
+    for (int i=0; i<islemListesi.size(); i++)
+    {
+        islemListesi[i].bilgiyazdir();
+    }
+}
+
+
+
+double FinanceManager::getanaBakiye(){return anaBakiye;}
--- FinanceManager.h
+++ FinanceManager.h
@@ -1,8 +1,24 @@
-//
-// Created by Emre on 13.06.2026.
-//
-
 #ifndef FINANS_ASISTANI_FINANCEMANAGER_H
 #define FINANS_ASISTANI_FINANCEMANAGER_H
+#include <vector>
+#include "Transaction.h"
+
+class FinanceManager
+{
+    private:
+        std::vector<Transaction> islemListesi;
+        double anaBakiye;
+
+    public:
+        //Yapıcı metot
+        FinanceManager();
+
+    void islemEkle(Transaction yeniIslem);
+    //Private kısmındaki bakiyeyi okuyabilmek için
+    double getanaBakiye();
+    //tüm işlemleri görebilmek için
+    void gecmisiListele();
+
+};
 
 #endif //FINANS_ASISTANI_FINANCEMANAGER_H
